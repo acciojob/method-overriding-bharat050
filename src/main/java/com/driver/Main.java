03b5cf0fd1b2d30a7b08960969a44b33055a23db
Bharat <aimhigh050@gmail.com>
2023-06-20 13:45:06 +0530

    first attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,18 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args){
+        B obj = new B();
+        String val =obj.meth();
+        System.out.println(val);
+    }
+
+}
+class A{
+    String meth(){
+        return "Inside class A";
+    }
+}
+class B extends A{
+
 }
